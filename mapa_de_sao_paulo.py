46c03353eb10914583658551f603000b26724a0c
KeichiTS <kkeichi@gmail.com>
2024-04-02 10:58:16 -0300

    geojson_e_codigo_mapa_de_sao_paulo

---
+++ mapa_de_sao_paulo.py
@@ -0,0 +1,26 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Ler o GeoJSON como um GeoDataFrame
+gdf_bairros = gpd.read_file("distritos-sp.geojson")
+
+# Definir as densidades populacionais para os bairros
+densidades = {
+    "ITAQUERA": 500,
+    "PINHEIROS": 1000,
+    "BUTANTA": 1500
+}
+
+# Atribuir as densidades populacionais aos bairros correspondentes no GeoDataFrame
+for bairro, densidade in densidades.items():
+        gdf_bairros.loc[gdf_bairros['ds_nome'] == bairro, 'Densidade_Populacional'] = densidade
+        
+gdf_bairros['Densidade_Populacional'] = gdf_bairros['Densidade_Populacional'].fillna(0)
+
+# Plotar o mapa
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+gdf_bairros.plot(column='Densidade_Populacional', cmap='OrRd', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+plt.title('Mapa de São Paulo')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.show()
